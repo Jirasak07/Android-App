4109df0c4fe4497e97ae6397c69d3b2bcaba8c71
Jirasak <102783958+Jirasak07@users.noreply.github.com>
2023-02-09 19:23:56 +0700

    delete old file

--- App.js
+++ App.js
@@ -3,17 +3,7 @@ import { useRef } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import LoginPage from "./Login/LoginPage";
-import TabBottom from "./BottomTab/BottomTab";
-
 
 export default function App() {
-  const First = createStackNavigator();
-  return (
-    <NavigationContainer>
-      <First.Navigator initialRouteName="login">
-        <First.Screen name="login" component={LoginPage} />
-        <First.Screen name="content" component={TabBottom} options={{headerTitile:'',tabBarLabel: "หน้าแรก",}} />
-      </First.Navigator>
-    </NavigationContainer>
-  );
+  return <LoginPage />;
 }
--- Tab/Tab-home.js
+++
@@ -1,9 +0,0 @@
-import react from "react";
-import { Text, View } from "react-native";
-export default function TabHome({navigation}) {
-  return (
-    <View>
-      <Text>TabHome</Text>
-    </View>
-  );
-}
