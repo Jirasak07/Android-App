e57bef509b0ea536a6b0393012dd9192cab3e058
my git btoiandw <bbdbiw2544@gmail.com>
2023-02-10 13:26:01 +0700

    header list booking

--- Page/RequestMe.js
+++ Page/RequestMe.js
@@ -2,16 +2,88 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Card } from "react-native-shadow-cards";
 import FaIcon from "react-native-vector-icons/FontAwesome5";
+import { NativeBaseProvider, Box } from "native-base";
 const RequestMe = () => {
   return (
     <View style={styles.container}>
       <View style={styles.content}>
-        <Card style={styles.card}>
+        <Card style={styles.cardAll}>
           <View style={styles.icon}>
-            <FaIcon name="clipboard-list" style={{ fontSize: 50 }} />
+            <FaIcon
+              name="clipboard-list"
+              style={{ fontSize: 50, color: "#fff",textAlign:'center', }}
+            />
           </View>
           <View style={styles.text_content}>
-            <Text>RequestMe</Text>
+            <Text style={{ color: "#fff", fontSize: 14, fontWeight: "300",textAlign:'center' }}>
+              ทั้งหมด
+            </Text>
+            <Text style={{ color: "#fff", fontSize: 30, fontWeight: "700",textAlign:'center', }}>
+              0
+            </Text>
+            <Text style={{ color: "#fff", fontSize: 14, fontWeight: "300",textAlign:'center' }}>
+              รายการ
+            </Text>
+          </View>
+        </Card>
+
+        <Card style={styles.cardPadding} >
+          <View style={styles.icon}>
+            <FaIcon
+              name="hourglass-end"
+              style={{ fontSize: 50, color: "#fff",textAlign:'center', }}
+            />
+          </View>
+          <View style={styles.text_content}>
+            <Text style={{ color: "#fff", fontSize: 14, fontWeight: "300",textAlign:'center' }}>
+              รอดำเนินการ
+            </Text>
+            <Text style={{ color: "#fff", fontSize: 30, fontWeight: "700",textAlign:'center', }}>
+              0
+            </Text>
+            <Text style={{ color: "#fff", fontSize: 14, fontWeight: "300",textAlign:'center' }}>
+              รายการ
+            </Text>
+          </View>
+        </Card>
+
+        <Card style={styles.cardApprove} >
+          <View style={styles.icon}>
+            <FaIcon
+              name="check-circle"
+              style={{ fontSize: 50, color: "#fff",textAlign:'center', }}
+            />
+          </View>
+          <View style={styles.text_content}>
+            <Text style={{ color: "#fff", fontSize: 14, fontWeight: "300",textAlign:'center' }}>
+              อนุมัติแล้ว
+            </Text>
+            <Text style={{ color: "#fff", fontSize: 30, fontWeight: "700",textAlign:'center', }}>
+              0
+            </Text>
+            <Text style={{ color: "#fff", fontSize: 14, fontWeight: "300",textAlign:'center' }}>
+              รายการ
+            </Text>
+          </View>
+        </Card>
+
+        <Card style={styles.cardCancel} >
+          <View style={styles.icon}>
+            <FaIcon
+              name="ban"
+              style={{ fontSize: 50, color: "#fff",textAlign:'center', }}
+            />
+          </View>
+          <View style={styles.text_content}>
+            <Text style={{ color: "#fff", fontSize: 14, fontWeight: "300",textAlign:'center' }}>
+              ยกเลิกแล้ว
+            </Text>
+            <Text style={{ color: "#fff", fontSize: 30, fontWeight: "700",textAlign:'center', }}>
+              0
+            </Text>
+            <Text style={{ color: "#fff", fontSize: 14, fontWeight: "300",textAlign:'center' }}>
+              รายการ
+            </Text>
           </View>
         </Card>
       </View>
@@ -24,23 +96,51 @@ export default RequestMe;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexWrap: "wrap",
     alignItems: "center",
     backgroundColor: "#EFF2EF",
   },
   content: {
     marginTop: 50,
   },
-  card: {
+  cardAll: {
     padding: 10,
-    margin: 10,
+    marginLeft:20,
+    marginBottom:10,
+    flexDirection: "row",
+    backgroundColor: "#4B3F72",
+  },
+  cardPadding: {
+    padding: 10,
+    marginLeft:20,
+    marginBottom:10,
+    flexDirection: "row",
+    backgroundColor: "#F2E94E",
+  },
+  cardCancel: {
+    padding: 10,
+    marginLeft:20,
+    marginBottom:10,
+    flexDirection: "row",
+    backgroundColor: "#DA3E52",
+  },
+  cardApprove: {
+    padding: 10,
+    marginLeft:20,
+    marginBottom:10,
+    flexDirection: "row",
+    backgroundColor: "#96E6B3",
   },
   icon: {
-    textAlign: "left",
-    padding: 10,
+    justifyContent: "center",
+    textAlign: "center",
+    flexGrow: 2,
+    color: "#fff",
   },
   text_content: {
-    textAlign: "right",
-    padding: 10,
-    alignSelf: "center",
+    flexGrow: 2,
+    justifyContent: "center",
+    textAlign: "center",
+    color: "#fff",
   },
 });
