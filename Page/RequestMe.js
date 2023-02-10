c19781e2b22cb8b2c5edb7cd8de95b1d0f094d19
my git btoiandw <bbdbiw2544@gmail.com>
2023-02-10 16:58:47 +0700

    vf

--- Page/RequestMe.js
+++ Page/RequestMe.js
@@ -1,6 +1,7 @@
-import { StyleSheet, Text, View,ScrollView } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 import React from "react";
 import RequestMeList from "./RequestMeList";
+import { Card } from "react-native-shadow-cards";
 const RequestMe = () => {
   return (
     <View style={styles.container}>
@@ -8,8 +9,13 @@ const RequestMe = () => {
         <RequestMeList />
       </View>
 
-      <ScrollView style={styles.contentH}>
-        <Text> </Text>
+      <ScrollView style={{ flex: 1 }}>
+        <View style={styles.contentH}>
+          <Text style={styles.text_h}>ข้อมูลการจอง</Text>
+          <Card style={styles.card}>
+            <Text>11111</Text>
+          </Card>
+        </View>
       </ScrollView>
     </View>
   );
@@ -18,11 +24,23 @@ const RequestMe = () => {
 export default RequestMe;
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    alignItems:'center',
+  container: {
+    flex: 1,
+    alignItems: "center",
   },
   contentH: {
     marginTop: 20,
+    height: 500,
+  },
+  card: {
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: "#FFF7F8",
+  },
+  text_h: {
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "700",
+    marginBottom: 10,
   },
 });
