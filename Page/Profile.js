f2bd53cc218bdc40623f48d5be2cac00cf7aa3b6
btoiandw <102774353+btoiandw@users.noreply.github.com>
2023-02-16 09:48:55 +0700

    d

--- Page/Profile.js
+++ Page/Profile.js
@@ -1,15 +1,89 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import globalStyles from "../Style/globalStyle";
+import Fa5Icon from "react-native-vector-icons/FontAwesome5";
+import FaIcon from "react-native-vector-icons/FontAwesome";
 
 const Profile = () => {
   return (
-    <View style={globalStyles.containerContent}>
-      <Text>Profile</Text>
+    <View style={{ flex: 1 }}>
+      <View
+        style={[
+          globalStyles.containerContent,
+          { paddingHorizontal: 10, paddingVertical: 10 },
+        ]}
+      >
+        <Text
+          style={{
+            fontSize: 20,
+            fontWeight: "700",
+          }}
+        >
+          PROFILE
+        </Text>
+        <View style={styles.content}>
+          <Fa5Icon
+            name="user-alt"
+            size={40}
+            style={{
+              padding: 50,
+              backgroundColor: "#ccc5b9",
+              borderRadius: 100,
+              color: "#111",
+            }}
+          />
+          <Text style={{ marginTop: 20, fontWeight: "900", fontSize: 32 }}>
+            Name
+          </Text>
+        </View>
+      </View>
+      <View>
+        <Pressable style={styles.btnLogout}>
+          <Text
+            style={{
+              color: "#fff",
+              textAlign: "center",
+              fontWeight: "900",
+              fontSize: 20,
+            }}
+          >
+            LOGOUT
+          </Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
 
 export default Profile;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  content: {
+    marginTop: 20,
+    justifyContent: "center",
+    borderBottomColor: "#403d39",
+    borderBottomWidth: 0.5,
+    alignItems: "center",
+    padding: 30,
+  },
+  btnLogout: {
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderRadius: 8,
+    backgroundColor: "#df3b57",
+    alignSelf: "center",
+    marginHorizontal: "1%",
+    marginBottom: 6,
+    minWidth: "50%",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+});
