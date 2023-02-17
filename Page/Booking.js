1d94085bc97db63367ad3779893c8e21e04a7f7f
btoiandw <102774353+btoiandw@users.noreply.github.com>
2023-02-17 12:59:09 +0700

    aa

--- Page/Booking.js
+++ Page/Booking.js
@@ -1,58 +1,65 @@
-import React, {Component} from 'react';
-import {Alert, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
-import {Agenda, DateData, AgendaEntry, AgendaSchedule} from 'react-native-calendars';
-import testIDs from '../testIDs';
-
+import React, { Component } from "react";
+import { Alert, StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import {
+  Agenda,
+  DateData,
+  AgendaEntry,
+  AgendaSchedule,
+} from "react-native-calendars";
+import testIDs from "../testIDs";
+import moment from "moment/moment";
 interface State {
   items?: AgendaSchedule;
 }
 
 export default class AgendaScreen extends Component<State> {
   state: State = {
-    items: undefined
+    items: undefined,
+  };
+  state = {
+    massage: { key: "massage", color: "#ffb703" },
+    workout: { key: "workout", color: "green" },
   };
 
-  state={
-    curDT : new Date().toLocaleString(),
-  }
-  // reservationsKeyExtractor = (item, index) => {
-  //   return `${item?.reservation?.day}${index}`;
-  // };
-
+  state = {
+    events: {
+      "2023-02-25": {
+        dots: [this.state.massage, this.state.workout],
+        title: "ไปสนามบิน",
+        start: "2023-02-25 10:00:00",
+        end: "2023-02-25 11:00:00",
+      },
+      "2023-02-26": {
+        dots: [this.state.massage, this.state.workout],
+        title: "ไปสนามบิน",
+        start: "2023-02-26 10:00:00",
+        end: "2023-02-26 11:00:00",
+      },
+    },
+  };
   render() {
     return (
       <Agenda
         testID={testIDs.agenda.CONTAINER}
         items={this.state.items}
         loadItemsForMonth={this.loadItems}
-        selected={this.state.curDT}
+        selected={moment(new Date()).format("YYYY-mm-dd")}
         renderItem={this.renderItem}
         renderEmptyDate={this.renderEmptyDate}
         rowHasChanged={this.rowHasChanged}
         showClosingKnob={true}
-        // markingType={'period'}
-        // markedDates={{
-        //    '2017-05-08': {textColor: '#43515c'},
-        //    '2017-05-09': {textColor: '#43515c'},
-        //    '2017-05-14': {startingDay: true, endingDay: true, color: 'blue'},
-        //    '2017-05-21': {startingDay: true, color: 'blue'},
-        //    '2017-05-22': {endingDay: true, color: 'gray'},
-        //    '2017-05-24': {startingDay: true, color: 'gray'},
-        //    '2017-05-25': {color: 'gray'},
-        //    '2017-05-26': {endingDay: true, color: 'gray'}}}
-        // monthFormat={'yyyy'}
-        // theme={{calendarBackground: 'red', agendaKnobColor: 'green'}}
-        //renderDay={(day, item) => (<Text>{day ? day.day: 'item'}</Text>)}
-        // hideExtraDays={false}
-        // showOnlySelectedDayItems
-        // reservationsKeyExtractor={this.reservationsKeyExtractor}
+        minDate={moment(new Date())}
+        markingType={"multi-dot"}
+        markedDates={this.state.events}
+        theme={{ calendarBackground: "#fff", agendaKnobColor: "#111" }}
       />
     );
   }
 
   loadItems = (day: DateData) => {
-    const items = this.state.items || {};
-
+    const items = this.state.events || {};
+    console.log(items);
+    //setTimeout(() => {}, 1500);
     setTimeout(() => {
       for (let i = -15; i < 85; i++) {
         const time = day.timestamp + i * 24 * 60 * 60 * 1000;
@@ -60,42 +67,42 @@ export default class AgendaScreen extends Component<State> {
 
         if (!items[strTime]) {
           items[strTime] = [];
-          
+
           const numItems = Math.floor(Math.random() * 3 + 1);
           for (let j = 0; j < numItems; j++) {
             items[strTime].push({
-              name: 'Item for ' + strTime + ' #' + j,
+              name: "Item for " + strTime + " #" + j,
               height: Math.max(50, Math.floor(Math.random() * 150)),
-              day: strTime
+              day: strTime,
             });
           }
         }
       }
-      
+
       const newItems: AgendaSchedule = {};
-      Object.keys(items).forEach(key => {
+      Object.keys(items).forEach((key) => {
         newItems[key] = items[key];
       });
       this.setState({
-        items: newItems
+        items: newItems,
       });
     }, 1000);
-  }
+  };
 
   renderItem = (reservation: AgendaEntry, isFirst: boolean) => {
     const fontSize = isFirst ? 16 : 14;
-    const color = isFirst ? 'black' : '#43515c';
+    const color = isFirst ? "black" : "#43515c";
 
     return (
       <TouchableOpacity
         testID={testIDs.agenda.ITEM}
-        style={[styles.item, {height: reservation.height}]}
+        style={[styles.item, { height: reservation.height }]}
         onPress={() => Alert.alert(reservation.name)}
       >
-        <Text style={{fontSize, color}}>{reservation.name}</Text>
+        <Text style={{ fontSize, color }}>{reservation.name}</Text>
       </TouchableOpacity>
     );
-  }
+  };
 
   renderEmptyDate = () => {
     return (
@@ -103,30 +110,31 @@ export default class AgendaScreen extends Component<State> {
         <Text>This is empty date!</Text>
       </View>
     );
-  }
+  };
 
   rowHasChanged = (r1: AgendaEntry, r2: AgendaEntry) => {
     return r1.name !== r2.name;
-  }
+  };
 
   timeToString(time: number) {
     const date = new Date(time);
-    return date.toISOString().split('T')[0];
+
+    return date.toISOString().split("T")[0];
   }
 }
 
 const styles = StyleSheet.create({
   item: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     flex: 1,
     borderRadius: 5,
     padding: 10,
     marginRight: 10,
-    marginTop: 17
+    marginTop: 17,
   },
   emptyDate: {
     height: 15,
     flex: 1,
-    paddingTop: 30
-  }
+    paddingTop: 30,
+  },
 });
